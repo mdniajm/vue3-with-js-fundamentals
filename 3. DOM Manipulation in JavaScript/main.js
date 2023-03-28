b673a048d2773bd9bc136b7a45d910065ea82a0b
mdniajm <mdniajm@gmail.com>
2023-03-28 19:40:17 +0600

    dtails about dom

---
+++ 3. DOM Manipulation in JavaScript/main.js
@@ -0,0 +1,27 @@
+let result;
+
+result = document.body;
+result = document.head;
+result = document.URL;
+result = document.domain;
+result = document.contentType;
+result = document.characterSet;
+result = document.doctype;
+result = document.forms;
+result = document.forms[0];
+result = document.forms[0].id;
+result = document.forms[0].method;
+result = document.forms[0].action;
+result = document.links;
+result = document.links[0];
+result = document.links[0].id;
+result = document.links[0].className;
+result = document.links[0].classList;
+result = document.links[0].classList[0];
+result = document.images;
+result = document.scripts;
+result = document.scripts[2].getAttribute('src');
+result = document.scripts[2].getAttribute('type');
+result = Array.from(document.scripts).map(script => script.getAttribute('src'));
+
+console.log(result);
