7ee8a5d744d029725ba7c89b2112904f79dd756b
mdniajm <mdniajm@gmail.com>
2023-03-28 19:49:07 +0600

    manupulate the style

--- 3. DOM Manipulation in JavaScript/main.js
+++ 3. DOM Manipulation in JavaScript/main.js
@@ -4,5 +4,13 @@ document.getElementById()
 */
 
 let val = document.getElementById('t-title');
-
 console.log(val);
+
+/*Get element item details */
+console.log(val.id);
+console.log(val.className);
+console.log(val.classList);
+
+/*Changing the styles */
+val.style.background = 'red';
+val.style.textAlign = 'center';
