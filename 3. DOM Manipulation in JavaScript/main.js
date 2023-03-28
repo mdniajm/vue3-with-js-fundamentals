969d5b45c1b0cf14a40273a8a78a2507fa127bb6
mdniajm <mdniajm@gmail.com>
2023-03-28 19:45:36 +0600

    select by js

--- 3. DOM Manipulation in JavaScript/main.js
+++ 3. DOM Manipulation in JavaScript/main.js
@@ -1,31 +1,8 @@
-let result;
+/*
+document.getElementById()
 
-result = document.body;
-result = document.head;
-result = document.URL;
-result = document.domain;
-result = document.contentType;
-result = document.characterSet;
-result = document.doctype;
-result = document.forms;
-result = document.forms[0];
-result = document.forms[0].id;
-result = document.forms[0].method;
-result = document.forms[0].action;
-result = document.links;
-result = document.links[0];
-result = document.links[0].id;
-result = document.links[0].className;
-result = document.links[0].classList;
-result = document.links[0].classList[0];
-result = document.images;
-result = document.scripts;
-result = document.scripts[2].getAttribute('src');
-result = document.scripts[2].getAttribute('type');
-result = Array.from(document.scripts);
+*/
 
-result.forEach(element => {
-    console.log(result);
-});
+let val = document.getElementById('t-title');
 
-console.log(result);
+console.log(val);
