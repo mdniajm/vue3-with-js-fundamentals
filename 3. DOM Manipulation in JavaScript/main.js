b059f5ce23b804b01d0a74bdc3e41f7420c5d7cb
mdniajm <mdniajm@gmail.com>
2023-04-03 19:21:31 +0600

    target btn details

--- 3. DOM Manipulation in JavaScript/main.js
+++ 3. DOM Manipulation in JavaScript/main.js
@@ -1,7 +1,15 @@
-
-
-
 /*Event Listener */
+const btn = document.querySelector('#addButton');
 
+//console.log(btn);
+
+btn.addEventListener('click', (event) => {
+    console.log(
+        event.target.id, 
+        event.target.className, 
+        event.target.classList, 
+        event.type);
+    event.preventDefault();
+});
 
 
