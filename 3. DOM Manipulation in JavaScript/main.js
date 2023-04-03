2d6e81cad821deb767674543d5d1a94388ebe4c7
mdniajm <mdniajm@gmail.com>
2023-04-03 19:43:04 +0600

    more evetn

--- 3. DOM Manipulation in JavaScript/main.js
+++ 3. DOM Manipulation in JavaScript/main.js
@@ -3,13 +3,24 @@ const btn = document.querySelector('#addButton');
 
 //console.log(btn);
 
-btn.addEventListener('click', (event) => {
-    console.log(
-        event.target.id, 
-        event.target.className, 
-        event.target.classList, 
-        event.type);
-    event.preventDefault();
-});
+// btn.addEventListener('click', (event) => {
+//     console.log(
+//         event.target.id, 
+//         event.target.className, 
+//         event.target.classList, 
+//         event.type,
+//         event.timeStamp,
+//         event.clientX, event.clientY,
+//         event.offsetX, event.offsetY
+        
+//         );
+//     event.preventDefault();
+// });
 
+const title = document.querySelector('#t-title');
+
+btn.addEventListener('mousemove', (event) => {
+    title.textContent = 'Changed Title';
+    title.style.color = 'red';
+});
 
