3c897374caaeef3d01b9dc74b94a9b24315c266e
mdniajm <mdniajm@gmail.com>
2023-04-05 22:24:34 +0600

    async and await concept

--- 5. Async JavaScript/Fetch API/main.js
+++ 5. Async JavaScript/Fetch API/main.js
@@ -1,61 +1,77 @@
-const btnText = document.getElementById('button-txt');
-const btnJSON = document.getElementById('button-json');
-const btnAPI = document.getElementById('button-api');
+/* Fetch data form text json and api */
 
-const post = document.getElementById('post');
+// const btnText = document.getElementById('button-txt');
+// const btnJSON = document.getElementById('button-json');
+// const btnAPI = document.getElementById('button-api');
 
-btnText.addEventListener('click', () =>{
-    fetch('data.txt')
-    .then((res) => {
-        console.log(res);
-        return res.text();
-    } ).then ((data) => {
-        console.log(data);
-        post.innerHTML = data;
-    }).catch((err) => {
-        console.log(err);
-    })
-});
+// const post = document.getElementById('post');
+
+// btnText.addEventListener('click', () =>{
+//     fetch('data.txt')
+//     .then((res) => {
+//         console.log(res);
+//         return res.text();
+//     } ).then ((data) => {
+//         console.log(data);
+//         post.innerHTML = data;
+//     }).catch((err) => {
+//         console.log(err);
+//     })
+// });
 
 
-btnJSON.addEventListener('click', () => {
-     fetch('posts.json')
-     .then((res) => {
-        return res.json();
-     })
-     .then ((data) => {
-        console.log(data);
-        let output = '';
-        data.forEach(post =>{
-            output += `
-            <li> ${post.title} </li>
+// btnJSON.addEventListener('click', () => {
+//      fetch('posts.json')
+//      .then((res) => {
+//         return res.json();
+//      })
+//      .then ((data) => {
+//         console.log(data);
+//         let output = '';
+//         data.forEach(post =>{
+//             output += `
+//             <li> ${post.title} </li>
 
-            `
-        })
-        post.innerHTML = output;
-     })
-     .catch ((err) => {
-        console.log(err);
-     })
-})
+//             `
+//         })
+//         post.innerHTML = output;
+//      })
+//      .catch ((err) => {
+//         console.log(err);
+//      })
+// })
 
-btnAPI.addEventListener('click', () =>{
-    fetch ('https://jsonplaceholder.typicode.com/todos')
-    .then((res) => {
-        return res.json();
-    })
-    .then ((data) => {
-        console.log(data);
-        let output = '';
-        data.forEach(post =>{
-            output += `
-            <li> ${post.title} </li>
+// btnAPI.addEventListener('click', () =>{
+//     fetch ('https://jsonplaceholder.typicode.com/todos')
+//     .then((res) => {
+//         return res.json();
+//     })
+//     .then ((data) => {
+//         console.log(data);
+//         let output = '';
+//         data.forEach(post =>{
+//             output += `
+//             <li> ${post.title} </li>
 
-            `
-        })
-        post.innerHTML = output;
-    })
-});
+//             `
+//         })
+//         post.innerHTML = output;
+//     })
+//     .catch ((err) => {
+//         console.log(err);
+//     })  
+// });
+
+/*Async and Await */
+
+async function fetchUsers() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    console.log(data);
+    
+}
+
+fetchUsers();
 
 
 
