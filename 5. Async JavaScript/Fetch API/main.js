e0289e13e33c3f283d6613e59d3caffbc81e756b
mdniajm <mdniajm@gmail.com>
2023-04-05 19:56:08 +0600

    get text using fetch

--- 5. Async JavaScript/Fetch API/main.js
+++ 5. Async JavaScript/Fetch API/main.js
@@ -2,14 +2,20 @@ const btnText = document.getElementById('button-txt');
 const btnJSON = document.getElementById('button-json');
 const btnAPI = document.getElementById('button-api');
 
+const post = document.getElementById('post');
+
 btnText.addEventListener('click', () =>{
     fetch('data.txt')
     .then((res) => {
+        console.log(res);
         return res.text();
     } ).then ((data) => {
         console.log(data);
+        post.innerHTML = data;
     }).catch((err) => {
         console.log(err);
     })
-})
+});
+
+
 
