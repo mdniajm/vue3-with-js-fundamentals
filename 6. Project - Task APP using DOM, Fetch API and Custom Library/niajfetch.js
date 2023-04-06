c62a07bc42f789cb2a96130444645e9e24521b5a
mdniajm <mdniajm@gmail.com>
2023-04-06 22:30:40 +0600

    POST request added

--- 6. Project - Task APP using DOM, Fetch API and Custom Library/niajfetch.js
+++ 6. Project - Task APP using DOM, Fetch API and Custom Library/niajfetch.js
@@ -12,6 +12,22 @@ class niajHTTP{
 
 
     /* POST REQUEST */
+    async post (url_link, data){
+        const res = await fetch(url_link, {
+            method: 'POST',
+            headers: {
+                'Content-type' : 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+
+        const resData = await res.json();
+        return resData;
+    }
+
+
+
+
     /* PUT REQUEST */
     /* DELETE REQUEST */
 }
