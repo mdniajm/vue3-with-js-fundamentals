278def9e26efe3a7f096f5916278aa18483100b6
mdniajm <mdniajm@gmail.com>
2023-04-10 20:00:09 +0600

    basic count app created

--- 8. Getting Started with Vue JS 3/first-project/src/App.vue
+++ 8. Getting Started with Vue JS 3/first-project/src/App.vue
@@ -1,4 +1,24 @@
 <template>
-  <h1>Vue JS 3 MasterClass</h1>
+  <h1>{{ courseName }}</h1>
+  <p>Course By {{mentor}}</p>
+
+  <button @click="count++"> + </button>
+  <h3>{{ count }}</h3>
+  <button @click="count--"> - </button>
 
 </template>
+
+
+
+
+<script>
+export default{
+  data(){
+    return{
+      courseName: 'Vue JS 3 MasterClass',
+      mentor: 'Md Niaj Makdhum',
+      count: 0
+    }
+  }
+}
+</script>
