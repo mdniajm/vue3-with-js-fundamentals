a2c98c2f416998f0f792bda1279b0f6a4652b72a
mdniajm <mdniajm@gmail.com>
2023-04-27 15:11:40 +0600

    two way data binding

--- 9. Refs, Reactive and Non-Reactive Data/new-project/src/App.vue
+++ 9. Refs, Reactive and Non-Reactive Data/new-project/src/App.vue
@@ -2,7 +2,9 @@
   <h1>{{ staticString }}</h1>
   <h1>{{ course.courseName }}</h1>
   <p>Course By {{course.mentor}}</p>
-
+  Insert Initial Value: <input type="number" v-model="count">
+  <br/>
+  <br/>
   <button @click="count++"> + </button>
   <h3>{{ count }}</h3>
   <button @click="count--"> - </button>
@@ -14,6 +16,7 @@
 <script setup>
   import { ref, reactive } from 'vue';
   let count = ref(0);
+  count.value = 10;
   const staticString = 'New Vue JS 3 Course';
   let course = reactive({
     courseName: 'Vue 3',
