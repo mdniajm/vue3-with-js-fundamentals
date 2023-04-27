48b838cf5f64a346992821a3fe7897442f0ad7db
mdniajm <mdniajm@gmail.com>
2023-04-27 17:44:57 +0600

    Function Method

---
+++ 10. Methods, Computed and Watcher/new-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>{{ staticString }}</h1>
+  <h1>{{ course.courseName }}</h1>
+  <p>Course By {{course.mentor}}</p>
+  <button @click="increment(2)"> + </button>
+  <h3>{{ count }}</h3>
+  <button @click="decrement(1)"> - </button>
+
+</template>
+
+
+
+<script setup>
+  import { ref, reactive } from 'vue';
+  let count = ref(0);
+  const staticString = 'New Vue JS 3 Course- Method/Function Based Component';
+  let course = reactive({
+    courseName: 'Vue 3',
+    mentor: 'Md Niaj Makhdum'
+  })
+  
+  function increment(amount){
+    count.value += amount;
+  }
+
+  function decrement(amount){
+    count.value -= amount;
+  }
+
+
+
+</script>
+
