012af3db733c37db93cc2708eb0a03edf3fde6d8
mdniajm <mdniajm@gmail.com>
2023-04-27 14:56:28 +0600

    Static String

--- 9. Refs, Reactive and Non-Reactive Data/new-project/src/App.vue
+++ 9. Refs, Reactive and Non-Reactive Data/new-project/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <h1>{{ staticString }}</h1>
   <h1>{{ course.courseName }}</h1>
   <p>Course By {{course.mentor}}</p>
 
@@ -13,6 +14,7 @@
 <script setup>
   import { ref, reactive } from 'vue';
   let count = ref(0);
+  const staticString = 'New Vue JS 3 Course';
   let course = reactive({
     courseName: 'Vue 3',
     mentor: 'Md Niaj Makhdum'
