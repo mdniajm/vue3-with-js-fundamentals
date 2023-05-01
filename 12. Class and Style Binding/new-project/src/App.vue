7cdd612efc8147fdef304c71dd48247b72df8064
mdniajm <mdniajm@gmail.com>
2023-05-01 18:53:01 +0600

    class style binding

---
+++ 12. Class and Style Binding/new-project/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <h1 v-show="showBtn">{{ staticString }} <span v-text="textType"></span></h1>
+
+  <button @click="showBtn= !showBtn">Show/Hide</button>
+  <h1>{{ course.courseName }}</h1>
+  <p @click="mentorName">Course By {{ mentorFullName }}</p>
+  <button @click="increment(1)"> + </button>
+  <h3>{{ count }} - {{ checkEvenOdd }}</h3>
+  <button @click="decrement(1)"> - </button>
+
+  <p v-html="htmlType"></p>
+  <ul>
+    <li v-for="toDo in toDos"> {{ toDo }} </li>
+  </ul>
+
+</template>
+
+
+
+<script setup>
+  import { ref, reactive, computed, watch } from 'vue';
+  let count = ref(0);
+
+  let showBtn = ref(true);
+
+  let toDos = ref(['Learn Vue 3', 'Learn React', 'Learn Node JS']);
+
+  let textType = ref ('built in directives V-show');
+  let htmlType = ref (`<li> this is a list item </li>`)
+  const staticString = 'New Vue JS 3 Course- ';
+  let course = reactive({
+    courseName: 'Vue 3',
+    mentorFirstName: 'Md Niaj',
+    mentorLastName: 'Makhdum',
+  })
+  
+  const increment = amount => count.value += amount;
+  const decrement = amount => count.value -= amount;
+  const mentorName = () => course.mentor = "Md. Niaj Makhdum";
+
+  const mentorFullName = computed(() => {
+    return course.mentorFirstName + ' ' + course.mentorLastName;
+  });
+
+  const checkEvenOdd = computed(() => {
+
+    return count.value % 2 === 0 ? 'Even': 'Odd';
+  });
+
+  /* Watch event  */
+
+  watch(count, (newValue, oldValue) => {
+    console.log('New Value: ', newValue, 'Old Value: ', oldValue);
+  });
+  
+
+
+
+
+</script>
+
