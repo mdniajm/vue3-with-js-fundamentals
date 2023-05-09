851bf1ea830e3945fbf13262e7bec5173619faf2
mdniajm <mdniajm@gmail.com>
2023-05-09 18:56:06 +0600

    styling of weather app

--- 19. Project - Weather App/new-project/src/App.vue
+++ 19. Project - Weather App/new-project/src/App.vue
@@ -42,3 +42,78 @@ export default {
   }
 }
 </script>
+
+<template>
+  <div class="weather-app">
+    <!-- Weather app content -->
+  </div>
+</template>
+
+<style>
+.weather-app {
+  margin: 20px auto;
+  padding: 20px;
+  max-width: 500px;
+  background-color: #f5f5f5;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  text-align: center;
+}
+
+h1 {
+  margin-top: 0;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+label {
+  font-size: 1.2rem;
+  margin-bottom: 5px;
+}
+
+input[type="text"] {
+  padding: 10px;
+  font-size: 1.2rem;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  margin-bottom: 10px;
+}
+
+button[type="submit"] {
+  padding: 10px 20px;
+  font-size: 1.2rem;
+  border: none;
+  border-radius: 5px;
+  background-color: #007bff;
+  color: #fff;
+  cursor: pointer;
+}
+
+button[type="submit"]:hover {
+  background-color: #0069d9;
+}
+
+h2 {
+  margin-top: 0;
+}
+
+p {
+  font-size: 1.2rem;
+  margin-bottom: 5px;
+}
+
+.weather-data {
+  margin-top: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  text-align: left;
+}
+</style>
