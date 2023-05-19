3348ea2114e5b718179f8cbf00bf6bd0458c401a
mdniajm <mdniajm@gmail.com>
2023-05-19 19:00:29 +0600

    new

--- 23. Project- Calendar/new-project/src/components/Calendar.vue
+++ 23. Project- Calendar/new-project/src/components/Calendar.vue
@@ -1,34 +1,78 @@
-Vue.component('calendar', {
-    props: ['currentDate', 'events'],
+<template>
+    <div>
+      <h2>{{ currentDate.toLocaleString('default', { month: 'long', year: 'numeric' }) }}</h2>
+      <button @click="previousMonth">Previous</button>
+      <button @click="nextMonth">Next</button>
+      <table>
+        <thead>
+          <tr>
+            <th v-for="day in 7" :key="day">{{ day }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="week in calendarRows" :key="week">
+            <td v-for="day in 7" :key="day" @click="selectDate(day)">
+              <span :class="{ 'selected': isSelected(day) }">{{ getDayNumber(week, day) }}</span>
+              <ul>
+                <li v-for="event in getEventsForDate(day)" :key="event.id">{{ event.title }}</li>
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Calendar",
+    props: {
+      currentDate: {
+        type: Date,
+        required: true,
+      },
+      events: {
+        type: Array,
+        default: () => [],
+      },
+    },
     methods: {
       previousMonth() {
-        // Implement this method to go to the previous month
+        const prevMonth = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() - 1, 1);
+        this.$emit("update-date", prevMonth);
       },
       nextMonth() {
-        // Implement this method to go to the next month
+        const nextMonth = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1, 1);
+        this.$emit("update-date", nextMonth);
       },
-      selectDate(date) {
-        // Implement this method to select a date and display events
-      }
+      selectDate(day) {
+        // Implement this method to select a date and perform actions
+      },
+      isSelected(day) {
+        // Implement this method to determine if a date is selected
+      },
+      getDayNumber(week, day) {
+        // Implement this method to calculate the day number for each cell
+      },
+      getEventsForDate(day) {
+        // Implement this method to filter events for a specific date
+      },
     },
-    template: `
-      <div>
-        <h2>{{ currentDate.toLocaleString('default', { month: 'long', year: 'numeric' }) }}</h2>
-        <button @click="previousMonth">Previous</button>
-        <button @click="nextMonth">Next</button>
-        <table>
-          <thead>
-            <tr>
-              <th v-for="day in 7">{{ day }}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="week in 6">
-              <td v-for="day in 7">{{ day }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    `
-  });
+    computed: {
+      calendarRows() {
+        const firstDayOfMonth = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth(), 1);
+        const firstDayOfWeek = firstDayOfMonth.getDay();
+        const totalDays = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1, 0).getDate();
+        const rows = Math.ceil((totalDays + firstDayOfWeek) / 7);
+        return rows;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .selected {
+    font-weight: bold;
+  }
+  </style>
   
