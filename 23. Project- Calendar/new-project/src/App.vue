e874362b5b6797c94cb2ac10628cd627daf5a4f3
mdniajm <mdniajm@gmail.com>
2023-05-19 18:58:41 +0600

    calendar

---
+++ 23. Project- Calendar/new-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <calendar :current-date="currentDate" :events="events"></calendar>
+  </div>
+</template>
+
+<script>
+import Calendar from "./components/Calendar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Calendar,
+  },
+  data() {
+    return {
+      currentDate: new Date(),
+      events: [],
+    };
+  },
+  methods: {
+    addEvent() {
+      // Implement this method to add events to the array
+    },
+    removeEvent(event) {
+      // Implement this method to remove events from the array
+    },
+  },
+};
+</script>
---
+++ 23. Project- Calendar/new-project/src/components/Calendar.vue
@@ -0,0 +1,34 @@
+Vue.component('calendar', {
+    props: ['currentDate', 'events'],
+    methods: {
+      previousMonth() {
+        // Implement this method to go to the previous month
+      },
+      nextMonth() {
+        // Implement this method to go to the next month
+      },
+      selectDate(date) {
+        // Implement this method to select a date and display events
+      }
+    },
+    template: `
+      <div>
+        <h2>{{ currentDate.toLocaleString('default', { month: 'long', year: 'numeric' }) }}</h2>
+        <button @click="previousMonth">Previous</button>
+        <button @click="nextMonth">Next</button>
+        <table>
+          <thead>
+            <tr>
+              <th v-for="day in 7">{{ day }}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="week in 6">
+              <td v-for="day in 7">{{ day }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    `
+  });
+  
